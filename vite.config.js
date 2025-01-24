1a2aed429ca53c00c612fd2fe79e14d50b211e6c
vijayakumar-kumaran <vijayakumark6186@gmail.com>
2025-01-24 13:16:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://employeewsbackend.onrender.com',
+      '/api': 'https://empworkspace.onrender.com',
     },
   },
 })
