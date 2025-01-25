08e6490d299e96e915bf5ea266d9d4759a9255e7
vijayakumar-kumaran <vijayakumark6186@gmail.com>
2025-01-25 11:29:08 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,13 +20,15 @@ import ViewListIcon from '@mui/icons-material/ViewList';
 import HomeWorkTwoToneIcon from '@mui/icons-material/HomeWorkTwoTone';
 import EditAttributesOutlinedIcon from '@mui/icons-material/EditAttributesOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
+
 import { useSelector } from 'react-redux';
 
-const Sidebar = () => {
+const Sidebar = ({openSidebar, setOpenSidebar}) => {
   const [openEmployee, setOpenEmployee] = useState(false);
   const [openTask, setOpenTask] = useState(false);
   const [openLeave, setOpenLeave] = useState(false);
-  const [openSidebar, setOpenSidebar] = useState(true);
+  
   const user = useSelector((state) => state.user);
 
   const handleClickEmployee = () => {
@@ -50,49 +52,52 @@ const Sidebar = () => {
       <IconButton
         onClick={toggleSidebar}
         sx={{
-          position: 'absolute',
-          top: 20,
-          left: 20,
-          zIndex: 1000,
-          color: '#fff',
-          display: openSidebar ? 'none' : 'block',
+          position: 'fixed',
+          top: '16px', 
+          left: openSidebar ? '260px' : '16px', 
+          zIndex: 2000, // Higher z-index to place above all elements
+          color: '#fff', // Button color
+          backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+          '&:hover': {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Darker on hover
+          },
+          transition: 'left 0.3s ease-in-out', // Smooth transition when toggling
         }}
       >
-        <MenuIcon />
+        {openSidebar ? <ChevronLeftOutlinedIcon /> : <MenuIcon />}
       </IconButton>
 
       <Drawer
         sx={{
-          width: openSidebar ? 280 : 0,
+          width: openSidebar ? 0 : 5,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
-            width: openSidebar ? 280 : 0,
+            width: openSidebar ? 280 : 72,
             boxSizing: 'border-box',
             padding: '20px',
-            background: 'linear-gradient(180deg, rgb(65, 56, 74), rgb(49, 52, 59))', // Vibrant gradient background
+            background: 'linear-gradient(180deg, rgb(65, 56, 74), rgb(49, 52, 59))',
             color: '#fff',
             border: 'none',
             boxShadow: '2px 0px 10px rgba(0, 0, 0, 0.2)',
             transition: 'width 0.3s ease-in-out',
-            overflowY: 'auto', // Enable scrolling
-            // Stylish scrollbar
+            overflowY: 'hidden',
             '&::-webkit-scrollbar': {
-              width: '4px', // Scrollbar width
+              width: '4px',
             },
             '&::-webkit-scrollbar-thumb': {
-              background: 'rgba(255, 255, 255, 0.4)', // Scrollbar thumb color
-              borderRadius: '10px', // Rounded scrollbar thumb
+              background: 'rgba(255, 255, 255, 0.4)',
+              borderRadius: '10px',
             },
             '&::-webkit-scrollbar-thumb:hover': {
-              background: 'rgba(255, 255, 255, 0.6)', // Hover effect
+              background: 'rgba(255, 255, 255, 0.6)',
             },
             '&::-webkit-scrollbar-track': {
-              background: 'rgba(0, 0, 0, 0.2)', // Scrollbar track color
-              borderRadius: '10px', // Rounded scrollbar track
+              background: 'rgba(0, 0, 0, 0.2)',
+              borderRadius: '10px',
             },
           },
         }}
-        variant="permanent"
+        variant="persistent"
         anchor="left"
         open={openSidebar}
       >
